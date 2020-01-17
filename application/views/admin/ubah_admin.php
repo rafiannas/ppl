e942a41323c662483bbf6a25322d3b8e80f90d23
oktavianaayuputris <57790959+oktavianaayuputris@users.noreply.github.com>
2020-01-17 14:51:05 +0700

    Ubah admin
    Update ubah admin

--- application/views/admin/ubah_admin.php
+++ application/views/admin/ubah_admin.php
@@ -7,28 +7,15 @@
 
     <div class="row">
         <div class="col-lg-4">
-        <?= $this->session->flashdata('message');  ?>
+            <?= $this->session->flashdata('message');  ?>
             <form action="" method="post">
 
-                <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" class="form-control" id="nama" name="nama" value="<?= $user2['nama_user']; ?>">
-                    <?= form_error('nama', '<small class="text-danger pl-3">', '</small>');  ?>
-                </div>
-                <div class="form-group">
-                    <label for="nim">NIM/NIP</label>
-                    <input type="number" class="form-control" id="nim" name="nim" value="<?= $user2['nim_user']; ?>">
-                    <?= form_error('nim', '<small class="text-danger pl-3">', '</small>');  ?>
-                </div>
+
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email" class="form-control" id="email" name="email" value="<?= $user2['email']; ?>" readonly>
                 </div>
-                <div class="form-group">
-                    <label for="nope">No Hp</label>
-                    <input type="number" class="form-control" id="nope" name="nope" value="<?= $user2['no_hp']; ?>">
-                    <?= form_error('nope', '<small class="text-danger pl-3">', '</small>');  ?>
-                </div>
+
 
 
                 <div class="form-group" row justify-content-end>
